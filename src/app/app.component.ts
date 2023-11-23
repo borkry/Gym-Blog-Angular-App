b24dec80c092e32ca87c931a67c37c03134b3577
Krystian Borys <krystian.borys231@gmail.com>
2023-11-23 22:07:41 +0100

    Added new components (login, register, about)  Changes to be committed: 	new file:   src/app/about/about.component.css 	new file:   src/app/about/about.component.html 	new file:   src/app/about/about.component.spec.ts 	new file:   src/app/about/about.component.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.routes.ts 	new file:   src/app/login/login.component.css 	new file:   src/app/login/login.component.html 	new file:   src/app/login/login.component.spec.ts 	new file:   src/app/login/login.component.ts 	new file:   src/app/register/register.component.css 	new file:   src/app/register/register.component.html 	new file:   src/app/register/register.component.spec.ts 	new file:   src/app/register/register.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { PostsListComponent } from './posts-list/posts-list.component';
 import { AddPostComponent } from './add-post/add-post.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PostsListComponent, AddPostComponent, RouterLink],
+  imports: [CommonModule, RouterOutlet, PostsListComponent, AddPostComponent, RouterLink, AboutComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { AddPostComponent } from './add-post/add-post.component';
 import { AppComponent } from './app.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
@@ -13,5 +16,20 @@ export const routes: Routes = [
         path: 'posts',
         title: 'Lista postów',
         component: PostsListComponent,
+    },
+    {
+        path: 'about',
+        title: 'Kontakt',
+        component: AboutComponent,
+    },
+    {
+        path: 'login',
+        title: 'Logowanie',
+        component: LoginComponent,
+    },
+    {
+        path: 'register',
+        title: 'Rejestracja',
+        component: RegisterComponent,
     }
 ];
