df69912e08232bc150678c6de5f43cc67729ae94
Michal <jan766078@gmail.com>
2023-12-13 19:20:20 +0100

    Added main page and bmi calculator for

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -56,7 +56,7 @@ export const routes: Routes = [
         canActivate : [authGuard],
     },
     {
-        path: 'posts/:id', 
+        path: 'posts/:id',
         component: PostDetailsComponent
     },
     {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [
+    FormsModule
+  ],
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  weight: number = 0;
+  height: number = 0;
+  bmi: number = 0;
+  bmiCategory: string = '';
+
+  calculateBMI() {
+    if (this.height > 0 && this.weight > 0) {
+      // Przeliczenie wzrostu na metry
+      const heightInMeters = this.height / 100;
+
+      // Obliczanie BMI: waga (kg) / (wzrost (m) * wzrost (m))
+      this.bmi = this.weight / (heightInMeters * heightInMeters);
+
+      // Określenie kategorii BMI
+      this.bmiCategory = this.getBmiCategory(this.bmi);
+    }
+    console.log("Gaming Console");
+    console.log(this.weight);
+    console.log(this.height);
+  }
+
+  getBmiCategory(bmi: number): string {
+    if (bmi < 18.5) {
+      return 'Niedowaga';
+    } else if (bmi < 24.9) {
+      return 'Prawidłowa waga';
+    } else if (bmi < 29.9) {
+      return 'Nadwaga';
+    } else {
+      return 'Otyłość';
+    }
+  }
 }
